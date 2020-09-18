edbb7a15b8fcb9f0aa672716d06b0685ddf5dc1d
Parimala Vemula <parimala.thodupunoori@gmail.com>
2020-09-17 17:37:03 -0700

    algorithms

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,18 +2,78 @@
 
 import sys
 
+# Write a function `making_change` that receives as input an amount of money in cents as well as an array of coin denominations 
+# and calculates the total number of ways in which change can be made for the input
+# For example, `making_change(10)` should return 4, since there are 4 ways 
+# to make change for 10 cents using pennies, nickels, dimes, quarters, and half-dollars:
+
+#  1. We can make change for 10 cents using 10 pennies
+#  2. We can use 5 pennies and a nickel
+#  3. We can use 2 nickels
+#  4. We can use a single dime
+
+
 def making_change(amount, denominations):
   # Your code here
 
-  pass
+    names = {50: "halfdollar(s)", 25: "quarter(s)", 10: "dime(s)", 5: "nickel(s)", 1: "pennies"}
+    add = None
+    comb = []
+    if add:
+        comb.append(add)
+        i = 0
+    if amount == 0 or (i+1) == len(denominations):
+        if (i+1) == len(denominations) and amount > 0:
+            if amount % denominations[i]:
+                return 0
+            comb.append((amount/denominations[i], denominations[i]))
+            i += 1
+        while i < len(denominations):
+            comb.append((0, denominations[i]))
+            i += 1
+        print(" ".join("%d %s" % (n, names[c]) for (n, c) in comb))
+        return 1
+    cur = denominations[i]
+    return sum(count_combs(amount-x*cur, i+1, comb[:], (x, cur)) for x in range(0, int(amount/cur)+1))
+
+# cents = 50
+# denominations = [50, 25, 10, 5, 1]
+
+# print(making_change(cents, denominations))
+
+
+def making_change2(amount, denominations):
+    print('making_change2({},{}) called'.format(amount, denominations))
+    if amount < 0 or len(denominations) == 0:
+        return 0
+    elif amount == 0:
+        return 1
+    num_ways = 0
+    for denomination in denominations:
+        leftover_denominations = list(filter(lambda x: x != denomination, denominations))
+        for i in range(amount // denomination + 1):
+            leftover_amount = amount - i * denomination
+            num_ways += making_change2(leftover_amount, leftover_denominations)
+
+    print('making_change2({},{}) returns {}'.format(amount, denominations, num_ways))
+
+    return num_ways
 
 
 if __name__ == "__main__":
+
   # Test our your implementation from the command line
   # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        # print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
+        print("There are {ways} ways to make {amount} cents.".format(ways=making_change2(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
+
+
+
+
+
--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,10 +2,59 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+# For example, given
+# ```
+# [1, 7, 3, 4]
+# ```
+# your function should return
+# ```
+# [84, 12, 28, 21]
+# ``` 
+# by calculating
+# ```
+# [7*3*4, 1*3*4, 1*7*4, 1*7*3]
+
+
+import functools
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    # make an empty array
+    # exclude the index or value
+    # multiply nums
+    # append it to array
+    # new_arr = []
+    
+    newList = []
+    for i in range(len(arr)):
+        product = 1
+        for x in range(len(arr)):
+            product *= arr[x]
+        product /= arr[i]
+        newList.append(int(product))
+    return newList
 
-    pass
+
+print(product_of_all_other_numbers([2,5,8,11]))
+
+def product_of_all_other_numbers2(arr):
+    newList = []
+    for i in range(len(arr)):
+        product = functools.reduce(lambda accum, curr: curr * accum, arr) / arr[i]
+        newList.append(int(product))
+    return newList
+
+
+print(product_of_all_other_numbers2([2,5,8,11]))
+
+# Has anyone figured out a way of multiplying all numbers in a list without using a loop? 
+# Is it even possible?
+# well you could chop the list up into segments of size 1, 
+# multiply segments and pass the products up the stack recursively,
+# remember the associative property: Any group of three or more numbers multiplied together will end in the same product no matter the grouping
+# so: (a*b*c*d*e*f*g*h) = ((a*b) * (c*d)) * ((e*f) * (g* h))
 
 
 if __name__ == '__main__':
